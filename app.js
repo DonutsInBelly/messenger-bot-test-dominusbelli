d20dc70bd4e4c93416564859e375b554354aa5ee
Carlin Au <carlinau@gmail.com>
2016-05-18 14:55:34 -0400

    added working index.html

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var config = require('./config/config.js');
+var path = require('path');
 
 var app = express();
+// app.use(express.static(__dirname + 'views'));
+var port = process.env.PORT || 8080;
+
+app.get('/', function(req,res) {
+  res.sendFile(path.join(__dirname + '/views/index.html'));
+})
 
 app.get('/webhook', function(req, res){
   if(req.query['hub.verify_token'] === config.verify_token) {
@@ -10,4 +17,5 @@ app.get('/webhook', function(req, res){
   res.send('Error, wrong validation token');
 });
 
-app.listen(8080);
+app.listen(port);
+console.log('Server listening on port ' + port);
