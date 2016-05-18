3fff9e7a1dff2ff36bb1c3c0a8c9ea4a07c65694
Carlin Au <carlinau@gmail.com>
2016-05-18 15:40:33 -0400

    adding https functionality

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ var express = require('express');
 var config = require('./config/config.js');
 var path = require('path');
 var bodyParser = require('body-parser');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
 
+var options = {
+  key: fs.readFileSync(path.join('.permissions/bot.pem')),
+  cert: fs.readFileSync(path.join('.permissions/bot.cert'))
+};
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -19,5 +26,6 @@ app.get('/webhook', function(req, res){
   res.send('Error, wrong validation token');
 });
 
-app.listen(port);
+http.createServer(app).listen(port);
+https.createServer(options, app).listen(port);
 console.log('Server listening on port ' + port);
