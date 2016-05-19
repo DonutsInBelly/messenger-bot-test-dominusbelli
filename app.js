7f92a3994102f809d91a6ed652a48f4578b837a6
Carlin Au <carlinau@gmail.com>
2016-05-19 15:55:38 -0400

    lets not deal with certificates and openssl for now :|

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var config = require('./config/config.js');
 var path = require('path');
 var bodyParser = require('body-parser');
-var http = require('http');
+/*var http = require('http');
 var https = require('https');
 var fs = require('fs');
 
@@ -10,6 +10,7 @@ var options = {
   key: fs.readFileSync(path.join('.permissions/bot.pem')),
   cert: fs.readFileSync(path.join('.permissions/bot.cert'))
 };
+*/
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -25,7 +26,9 @@ app.get('/webhook', function(req, res){
   }
   res.send('Error, wrong validation token');
 });
-
+/*
 http.createServer(app).listen(port);
 https.createServer(options, app).listen(port);
+*/
+app.listen(port);
 console.log('Server listening on port ' + port);
